c9695a5d3398366885da33de49b3784769790e34
Rob Cameron <rwcameron@gmail.com>
2014-12-18 17:08:51 -0800

    adding in package files

--- setup.py
+++ setup.py
@@ -8,10 +8,18 @@ setup(
     version=pyJunosManager.__version__,
     description='A simplified module to handle common Junos tasks',
     url='https://github.com/JNPRAutomate/pyJunosManager',
-    license='???',
+    license='Apache 2.0',
     author='Rob Cameron',
     install_requires=["junos-eznc>=1.0.2","Jinja2>=2.7.3"],
     author_email='rcameron@juniper.net',
     packages=['pyJunosManager'],
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
