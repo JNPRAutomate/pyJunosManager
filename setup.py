b10af891700542b3257376bb43552844a312b98a
Rob Cameron <rwcameron@gmail.com>
2014-12-22 12:24:00 -0800

    fixing docs setup

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-
 import pyJunosManager
+import os
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 setup(
     name='pyJunosManager',
